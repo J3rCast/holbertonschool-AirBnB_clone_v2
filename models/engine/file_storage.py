f5e2ba4f8c244ebc4dc8254aa4f6be68ed9333a6
J3rCast <jerson.castro.r@gmail.com>
2022-09-05 18:01:04 -0500

    bug fixed

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,7 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        if not cls or not self.__objects:
+        if cls == None or not self.__objects:
             return self.__objects
         else:
             new_list = {}
