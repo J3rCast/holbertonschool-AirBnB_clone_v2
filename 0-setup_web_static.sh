44bc884bc1963fec27ecd1a90abeb7d21e45b7f7
J3rCast <jerson.castro.r@gmail.com>
2022-09-11 15:32:29 -0500

    sudo keyword added

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,3 +12,4 @@ sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo rm -f /etc/nginx/modules-available/default
 sudo cp config_file /etc/nginx/sites-available/default
+sudo service nginx restart
