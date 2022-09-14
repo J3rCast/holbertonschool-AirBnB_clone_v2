c97cdb98322745f3ec7e63f9c6ad5bc6bdcd0535
J3rCast <jerson.castro.r@gmail.com>
2022-09-14 08:51:17 -0500

    tasks added

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,9 @@ from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/")
+@app.route("/", strict_slashes = False)
 def hello_hbnb():
     """Display a web aplication."""
     return "Hello HBNB!"
